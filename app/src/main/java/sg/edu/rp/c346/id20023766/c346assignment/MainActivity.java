5c1d86a357608c8669dee1d1bdba0af75bd818d7
weihong <20023766@rp.edu.sg>
2021-06-24 14:48:36 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id20023766/c346assignment/MainActivity.java
@@ -0,0 +1,17 @@
+package sg.edu.rp.c346.id20023766.c346assignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button github;
+    Button app2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
